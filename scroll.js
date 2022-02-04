88fa7adc0dcd423271a7c378bd79a13469063cba
ThemeSchemer <94884864+themeschemer@users.noreply.github.com>
2022-02-04 15:34:33 -0600

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -7,6 +7,7 @@ function scroll() {
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("scroll").style.width = scrolled + "%";
+  document.title = scrolled + "%";
   document.getElementById("progress").innerHTML = Math.round(scrolled) + "%";
   if(Math.round(scrolled) >= 9) {
     document.getElementById("scroll-div").style.position = "fixed";
